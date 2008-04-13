7b9ee4c9861c6e38d0bb51f17f9bcc15cc383870
Andy Armstrong <andy@hexten.net>
2008-04-13 14:09:55 +0000

    Added

---
+++ lib/WWW/MySociety/Gaze.pm
@@ -0,0 +1,62 @@
+package WWW::MySociety::Gaze;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+WWW::MySociety::Gaze - [One line description of module's purpose here]
+
+=head1 VERSION
+
+This document describes WWW::MySociety::Gaze version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use WWW::MySociety::Gaze;
+  
+=head1 DESCRIPTION
+
+=head1 INTERFACE 
+
+=head2 C<< somfunc >>
+
+=cut
+
+1;
+__END__
+
+=head1 CONFIGURATION AND ENVIRONMENT
+  
+WWW::MySociety::Gaze requires no configuration files or environment variables.
+
+=head1 DEPENDENCIES
+
+None.
+
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-www-mysociety-gaze@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
+=head1 AUTHOR
+
+Andy Armstrong  C<< <andy@hexten.net> >>
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2008, Andy Armstrong C<< <andy@hexten.net> >>.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
